c9183d15c28606d0eb1ad05bd7f1bf2984256584
electem <vinzy@electems.com>
2026-04-18 16:13:55 +0530

    d

--- SpringBoot_ApiKey/src/main/java/com/springboot_apikey/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot_apikey.repository;
-
-import com.springboot_apikey.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
-}
