04f829403533142f5adefa7fa665d4f8d67c3da3
electem <vinzy@electems.com>
2026-04-18 16:19:57 +0530

    Commit the project

---
+++ SpringBoot_ApiKey/src/main/java/com/springboot_apikey/SpringBootApiKeyApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_apikey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiKeyApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_ApiKey/src/main/java/com/springboot_apikey/controller/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.springboot_apikey.controller;
+
+import com.springboot_apikey.entity.Employee;
+import com.springboot_apikey.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+        private final EmployeeService service;
+
+        // CREATE
+        @PostMapping
+        public Employee createEmployee(@RequestBody Employee employee) {
+            return service.saveEmployee(employee);
+        }
+
+        // READ ALL
+        @GetMapping
+        public List<Employee> getAllEmployees() {
+            return service.getAllEmployees();
+        }
+
+        // READ BY ID
+        @GetMapping("/{id}")
+        public Employee getEmployee(@PathVariable Long id) {
+            return service.getEmployeeById(id)
+                    .orElseThrow(() -> new RuntimeException("Employee not found"));
+        }
+
+        // UPDATE
+        @PutMapping("/{id}")
+        public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+            return service.updateEmployee(id, employee);
+        }
+
+        @DeleteMapping("/{id}")
+        public String deleteEmployee(@PathVariable Long id) {
+                service.deleteEmployee(id);
+                return "Employee deleted successfully";
+        }
+
+}
