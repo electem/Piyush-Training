6dc7f7bc8fb127a17e495f1d5fa790a1cc84b1db
electem <vinzy@electems.com>
2026-04-21 18:17:36 +0530

    Update in my project

---
+++ externalApi/src/main/java/com/externalApi/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.externalApi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("External API")
+                        .version("1.0")
+                        .description("JWT Authentication API Documentation")
+                )
+
+                // 🔐 Add Security Requirement
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+
+                // 🔐 Define JWT Scheme
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
